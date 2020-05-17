1837d11da7d8433ab744c2690d137e8779747e1f
Chris Duehrsen <duehrsen@gmail.com>
2020-05-17 16:25:26 -0700

    Updated model to include ID identity data annotation.

--- WebApplication1/Models/EmailReportPair.cs
+++ WebApplication1/Models/EmailReportPair.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication1.Models
 {
     public class EmailReportPair
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public string Email { get; set; }
         public string ReportName { get; set; }
     }
 
-/*    public class EmailReportPairDBContext : DbContext
-    {
-        public DbSet<EmailReportPair> Pairs { get; set; }
-    }*/
 }
